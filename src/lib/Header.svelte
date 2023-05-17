44144984b3d84cae94c7f786791ac60505c5bbb5
Ayo Onamusi <onamusia@gmail.com>
2023-05-17 22:10:32 +0100

    lab14a-0.1.0

---
+++ src/lib/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+  import TitleBar from './TitleBar.svelte';
+  import WelcomeNavigator from './WelcomeNavigator.svelte';
+</script>
+
+<div class="columns is-vcentered">
+  <div class="column is-two-thirds">
+      <TitleBar title={'Donation Services Inc.'} subTitle={'Sign up or Log in'} />
+  </div>
+  <div class="column">
+      <slot> 
+        <WelcomeNavigator /> <!-- makes welcomeNavigator the default for the slot position -->
+      </slot>
+  </div>
+</div>
